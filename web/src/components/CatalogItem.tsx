bc401473eb1f1d0df334848b03a6b55311f00bfa
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-07-11 16:41:04 -0300

    initial commit - added react with redux

---
+++ web/src/components/CatalogItem.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
+
+import { addProductToCartRequest } from '../store/modules/cart/actions';
+import { IProduct } from '../store/modules/cart/types';
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+
+export function CatalogItem({ product }: CatalogItemProps){
+   const dispatch = useDispatch();
+
+   const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+     return state.cart.failedStockCheck.includes(product.id);
+   });
+
+   const handleAddProductToCart = useCallback((product: IProduct) => {
+      dispatch(addProductToCartRequest(product))
+   }, [dispatch])
+
+   return (
+      <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {" "}
+
+          <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
+
+          {hasFailedStockCheck && <span style={{color: 'red'}}>falta de estoque!</span>}
+      </article>
+   );
+}
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Catalog } from "../components/Catalog";
+import { Cart } from "../components/Cart";
+
+export default function Home() {
+  return (
+    <>
+      <Catalog /> 
+      <Cart />
+    </>
+  )
+}
